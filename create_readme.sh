8daed39816d3b4bbc066dad967a4ffe20e7a475c
guptaNswati <sgupta1505@gmail.com>
2017-03-12 07:56:15 +0000

    updated for login

--- create_readme.sh
+++ create_readme.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # This script creates README for my projects parsing projects html page.
+# First create save the file with lynx dump needing login, use lynx url
+file_to_parse=parse="/tmp/temp1.txt"
 file_to_create="/tmp/temp.txt"
-lynx -dump "$1" | grep -A20000 "Tasks" | tail -n+2 | grep -vE "BUTTON|Help|Score|Done|Examples|Example|example|review" | grep -E "Write|File|Directory|mandatory|advanced" >> $file_to_create
+lynx -dump $file_to_parse | grep -A20000 "Tasks" | tail -n+2 | grep -vE "BUTTON|Help|Score|Done|Examples|Example|example|review" | grep -E "Write|File|Directory|mandatory|advanced" >> $file_to_create
 tittle=$(grep -m1 "Directory" $file_to_create)
 sed -i '/Directory/d' $file_to_create
 sed -r -i 's/\b(mandatory|advanced)\b//g' $file_to_create
