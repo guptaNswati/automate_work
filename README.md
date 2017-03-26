b244ea30979d43466ceedf58f07e22ea967878a1
guptaNswati <sgupta1505@gmail.com>
2017-03-26 03:19:07 +0000

    updated instructions and format of script

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ This repository contains programs/scripts to help me automate my work.
 
 create_readme.sh - a bash script that creates README for my projects
 		 parsing projects html page using lynx.
-- use lynx projectURL
+- Usage:
+- Before running the script, type lynx projectURL
 - enter login credentials
-- enter P, file name /tmp/temp1.txt enter Q
-- needs execution permissions ``` chmod u+x create_readme.sh```
-- execute ./create_readme.sh with path to folder to save readme
+- enter p, click on save to local file, enter file name /tmp/temp1.txt enter q
+- Check the txt of html page in /tmp
+- to script give  execution permissions ``` chmod u+x create_readme.s h```
+- execute ./create_readme.sh with path to destination directory to save readme
--- create_readme.sh
+++ create_readme.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 # This script creates README for my projects parsing projects html page.
-# First create plain txt of project html page: use lynx url and enter login credentials
+# First create plain txt of project html page: use lynx url
+# enter login credentials, Press p and on click on "Save to local file"
+# enter /tmp/temp1.txt and press q, check plain txt of the html page in /tmp
+# run this file with path of readme destination directory
+
+
 file_to_parse="/tmp/temp1.txt"
 file_to_create="/tmp/temp.txt"
-lynx -dump $file_to_parse | grep -A20000 "Tasks" | tail -n+2 | grep -vE "BUTTON|Help|Score|Done|Examples|Example|example|review" | grep -E "mandatory|advanced|Write|Directory|File" >> $file_to_create
+filter_out="BUTTON|Help|Score|Done|Examples|Example|example|review"
+filter_in="mandatory|advanced|Write|Directory|File"
+
+
+lynx -dump $file_to_parse | grep -A20000 "Tasks" | tail -n+2 | grep -vE "$filter_out" | grep -E "$filter_in" >> $file_to_create
+
+
+# save directory name for  the heading of the readme
 tittle=$(grep -m1 "Directory" $file_to_create)
+
+
+# remove repetitive and unwanted lines
 sed -i '/Directory/d' $file_to_create
 sed -r -i 's/\b(mandatory|advanced)\b//g' $file_to_create
 sed -i "1s/^/$tittle\n\n /" $file_to_create
+
+
 # folder to save readme is specified in arg
 mv $file_to_create "$1/README.md"
+
+
 # to remove unwanted end lines
 # head -n -2 $file_to_create >> "$1/README.md"
